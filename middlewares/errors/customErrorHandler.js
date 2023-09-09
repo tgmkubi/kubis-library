21074e198fbbdc3fe0cd85234a1aded401d927bd
Kubilay Uysal <kubilayuysall@gmail.com>
2023-09-09 21:55:18 +0300

    Exprs CustomErrorHandler Mid. and CustomError Cls.

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -1,8 +1,24 @@
+const CustomError = require('../../helpers/error/CustomError');
+
 const customErrorHandler = (err, req, res, next) => {
-    console.log(err);
-    res.status(400).json({
-        success: false
-    })
+    //console.log(err.code);
+    //console.log(typeof err.code);
+    let customError = err;
+
+    if(err.name === "SyntaxError") {
+        customError = new CustomError(err.message, 400);
+    };
+    if(err.name === "ValidationError") {
+        customError = new CustomError(err.message, 400);
+    };
+    if(err.code === 11000) {
+        customError = new CustomError("Duplication Key Error", 400);
+    };
+
+    res.status(customError.status || 500).json({
+        success: false,
+        message: customError.message || "Internal Server Error"
+    });
   }
 
   module.exports = customErrorHandler;
