6d0ad53ee725862d0f256eafbf710d34f87c1c82
Kubilay Uysal <kubilayuysall@gmail.com>
2023-09-10 20:15:23 +0300

    Error Catch with express-async-handler npm package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,66 +1,82 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const {sendJwtToClient} = require('../helpers/authorization/tokenHelpers');
-const {validateUserInput, comparePassword} = require('../helpers/input/inputHelpers');
-const CustomError = require('../helpers/error/CustomError');
-const checkUserExist = require('../helpers/database/checkUserExist');
+const User = require("../models/User");
+const { sendJwtToClient } = require("../helpers/authorization/tokenHelpers");
+const {
+  validateUserInput,
+  comparePassword,
+} = require("../helpers/input/inputHelpers");
+const CustomError = require("../helpers/error/CustomError");
+const checkUserExist = require("../helpers/database/checkUserExist");
+const asyncErrorWrapper = require("express-async-handler");
 
-const register = async(req, res, next) => {
+const register = asyncErrorWrapper(async (req, res, next) => {
+  const { name, email, password, role, phonenumber } = req.body;
 
-    try {
-        const {name, email, password, role, phonenumber} = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    phonenumber,
+  });
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        role,
-        phonenumber
+  sendJwtToClient(user, res);
+});
+
+const login = asyncErrorWrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!validateUserInput(email, password)) {
+    return next(new CustomError("Please check your inputs", 400));
+  }
+
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!checkUserExist(user)) {
+    return next(new CustomError("There is no user with that email", 400));
+  }
+
+  const result = await comparePassword(password, user.password);
+  if (!result) {
+    return next(new CustomError("Please check your credentials", 400));
+  }
+
+  sendJwtToClient(user, res);
+});
+
+// const logout = async (req, res, next) => {
+//   res.status(200).clearCookie("access_token").json({
+//     success: true,
+//     message: "Logout Successful"
+//   });
+// };
+
+const logout = asyncErrorWrapper(async (req,res,next) =>{
+   
+    const {NODE_ENV} = process.env;
+    
+    // Send To Client With Res
+    
+    return res
+    .status(200)
+    .cookie("access_token",null, {
+        httpOnly : true,
+        expires : new Date(Date.now()),
+        secure : NODE_ENV === "development" ? false : true
+    })
+    .json({
+        success : true,
+        message : "Logout Successfull"
     });
     
-    sendJwtToClient(user, res);
-    
-    } catch (error) {
-        return next(error); // Express wll catch the error with next
-    } 
+});
+
+const getLoggedInUser = async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: req.user,
+  });
 };
 
-const login = async(req, res, next) => {
-
-    const {email, password} = req.body;
-
-    if(!validateUserInput(email, password)) {
-        return next(new CustomError("Please check your inputs", 400));
-    };
-
-    const user = await User.findOne({email: email}).select("+password");
-    
-    if(!checkUserExist(user)) {
-        return next(new CustomError("There is no user with that email", 400));
-    };
-
-    const result = await comparePassword(password, user.password);
-    if(!result) {
-        return next(new CustomError("Please check your credentials", 400));
-    }
-
-    sendJwtToClient(user, res);
-};
-
-const logout = async(req, res, next) => {
-
-    res.status(200).clearCookie('access_token').json({
-        success: true,
-        message: "Logout Successful"
-    });
-};
-
-const getLoggedInUser = async(req, res, next) => {
-    res.status(200).json({
-        success: true,
-        data: req.user
-    });
-};
-
-module.exports = {register, login, logout, getLoggedInUser};
+module.exports = { register, login, logout, getLoggedInUser };
--- routers/auth.js
+++ routers/auth.js
@@ -7,6 +7,6 @@ const {register, login, logout, getLoggedInUser} = require('../controllers/auth'
 router.post('/register', register);
 router.get('/login', login);
 router.get('/logout', getAccessToRoute, logout);
-router.get('/user',getAccessToRoute, getLoggedInUser);
+router.get('/profile',getAccessToRoute, getLoggedInUser);
 
 module.exports = router;
