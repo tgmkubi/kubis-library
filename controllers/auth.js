3e3e7f55613934f3ebb3018238bee9a3b17149a3
Kubilay Uysal <kubilayuysall@gmail.com>
2023-09-11 16:15:34 +0300

    Reset Password-Send reset password token to email

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,7 @@ const {
 const CustomError = require("../helpers/error/CustomError");
 const checkUserExist = require("../helpers/database/checkUserExist");
 const asyncErrorWrapper = require("express-async-handler");
+const sendEmail = require("../helpers/libraries/sendEmail");
 
 const register = asyncErrorWrapper(async (req, res, next) => {
   const { name, email, password, role, phonenumber } = req.body;
@@ -52,24 +53,22 @@ const login = asyncErrorWrapper(async (req, res, next) => {
 //   });
 // };
 
-const logout = asyncErrorWrapper(async (req,res,next) =>{
-   
-    const {NODE_ENV} = process.env;
-    
-    // Send To Client With Res
-    
-    return res
+const logout = asyncErrorWrapper(async (req, res, next) => {
+  const { NODE_ENV } = process.env;
+
+  // Send To Client With Res
+
+  return res
     .status(200)
-    .cookie("access_token",null, {
-        httpOnly : true,
-        expires : new Date(Date.now()),
-        secure : NODE_ENV === "development" ? false : true
+    .cookie("access_token", null, {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+      secure: NODE_ENV === "development" ? false : true,
     })
     .json({
-        success : true,
-        message : "Logout Successfull"
+      success: true,
+      message: "Logout Successfull",
     });
-    
 });
 
 const getLoggedInUser = async (req, res, next) => {
@@ -79,38 +78,70 @@ const getLoggedInUser = async (req, res, next) => {
   });
 };
 
-const imageUpload = asyncErrorWrapper(async (req,res,next) => {
-  const user = await User.findByIdAndUpdate(req.user.id, {
-    "profile_image": req.savedProfiledImage
-  }, {
-    new: true,
-    runValidators: true
-  });
+const imageUpload = asyncErrorWrapper(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      profile_image: req.savedProfiledImage,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
   res.status(200).json({
     success: true,
     message: "Image Upload Successful",
-    data: user
+    data: user,
   });
 });
 
-const forgotpassword = asyncErrorWrapper(async (req,res,next) => {
-  const {email} = req.body;
+const forgotpassword = asyncErrorWrapper(async (req, res, next) => {
+  const { email } = req.body;
 
-  const user = await User.findOne({email: email});
+  const user = await User.findOne({ email: email });
 
-  if(!checkUserExist(user)) {
+  if (!checkUserExist(user)) {
     return next(new CustomError("There is no user with that email", 400));
-  };
+  }
 
   const resetPasswordToken = user.getResetPasswordFromUser();
 
   await user.save();
 
-  res.status(200).json({
-    success: true,
-    token: resetPasswordToken
-  });
+  const { URL, PORT } = process.env;
+
+  const resetPasswordUrl = `${URL}${PORT}/api/auth/resetPassword?resetPasswordToken=${resetPasswordToken}`;
+
+  const emailTemplate = `
+    <h3>Reset Your Password</h3>
+    <p>This <a href = '${resetPasswordUrl}' target = "_blank">link</a> will expire in 1 hour.</p>
+  `
+  try {
+    await sendEmail({
+      from: process.env.SMTP_HOST,
+      to: email,
+      subject: "Question Answer Api - Reset Password",
+      html: emailTemplate
+    });
+    res.status(200).json({
+      success: true,
+      message: `Token sent to '${user.email}'`,
+    });
+  } catch (err) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    return next(new CustomError("Email could not be sent", 500));
+  }
 });
 
-module.exports = { register, login, logout, getLoggedInUser, imageUpload, forgotpassword};
+module.exports = {
+  register,
+  login,
+  logout,
+  getLoggedInUser,
+  imageUpload,
+  forgotpassword,
+};
