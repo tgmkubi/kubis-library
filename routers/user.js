bf39bc531327a38611aafa8d95033d92937a8297
Kubilay Uysal <kubilayuysall@gmail.com>
2023-09-12 10:38:29 +0300

    User Router Configiration

---
+++ routers/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: "User Page"
+    })
+});
+
+module.exports = router;
