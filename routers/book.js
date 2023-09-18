c8a8976d54cabb4243cd8af885f82ad5e7ab82f6
Kubilay Uysal <kubilayuysall@gmail.com>
2023-09-18 11:45:33 +0300

    Register A Book

--- routers/book.js
+++ routers/book.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const Book = require('../models/Book');
 const asyncErrorWrapper = require('express-async-handler');
 
-router.get('/', (req, res, next) => {
+router.post('/register', asyncErrorWrapper(async(req, res, next) => {
+    const newBook = req.body;
+
+    const book = await Book.create(newBook);
+
     res.status(200).json({
         success: true,
-        message: "Book Route successful"
+        data: book
     })
-});
+}));
 
 module.exports = router;
